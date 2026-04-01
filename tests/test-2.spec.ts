f79db6f24b8e1deccb1cb99bffdb375e5288f738
Davide Palma (ElDavo) <github@davidepalma.it>
2026-04-01 11:58:54 +0200

    switch to google_photos_mobile_client implementation

--- tests/test-2.spec.ts
+++
@@ -1,7 +0,0 @@
-import { test, expect } from '@playwright/test';
-
-test('test', async ({ page }) => {
-  await page.goto('https://www.google.com/photos/about/');
-  await page.getByRole('link', { name: 'Download the Google Photos app' }).click();
-  await page.getByRole('link', { name: 'Download the Google Photos app' }).click();
-});
